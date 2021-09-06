58eb6b460d1363b89397d1430ed501311d3e5ebf
adehcby <3533258769@qq.com>
2021-09-07 00:31:20 +0800

    added first item

---
+++ src/main/java/net/adehcby/mcga/McgaMain.java
@@ -0,0 +1,15 @@
+package net.adehcby.mcga;
+
+import net.adehcby.mcga.registry.ItemInit;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(McgaMain.MODID)
+public class McgaMain {
+    public static final String MODID = "mcga";
+
+    public McgaMain()
+    {
+        ItemInit.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+}
